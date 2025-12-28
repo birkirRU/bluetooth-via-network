9332971be96e3d89184101da7b18d19e6842a83a
Birkir <birkirsa24@ru.is>
2025-12-28 14:42:48 +0000

    s

--- AudioReciever/reciever.py
+++ AudioReciever/reciever.py
@@ -5,9 +5,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('0.0.0.0', 5000))
 
 print("waiting for message")
-
+request_count: int = 0
 while True:
     max_buffer_size: int = 35280
     data, addr = sock.recvfrom(max_buffer_size)
     print(f"Bytes")
+    request_count+=1
+    print(request_count)
     print()
